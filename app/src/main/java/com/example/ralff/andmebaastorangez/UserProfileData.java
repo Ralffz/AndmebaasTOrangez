17f770ff8c6de8143c49322a8857840c449ead9e
Ralffz <Rabel141@gmail.com>
2018-04-09 12:52:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ralff/andmebaastorangez/UserProfileData.java
@@ -0,0 +1,42 @@
+package com.example.ralff.andmebaastorangez;
+
+/**
+ * Created by Ralff on 4/8/2018.
+ */
+
+public class UserProfileData {
+    public String eesNimi;
+    public  String pereNimi;
+    public String epost;
+
+    public UserProfileData(String eesNimi, String pereNimi, String epost) {
+        this.eesNimi = eesNimi;
+        this.pereNimi = pereNimi;
+        this.epost = epost;
+    }
+
+    public UserProfileData (){
+
+    }
+    public String getEesNimi(){
+        return eesNimi;
+    }
+    public void getEesNimi(String eesNimi){
+        this.eesNimi = eesNimi;
+    }
+    public String getPereNimi(){
+        return pereNimi;
+    }
+    public void getPereNimi(String pereNimi){
+        this.pereNimi = pereNimi;
+    }
+    public String getEpost(){
+        return epost;
+    }
+    public void getEpost(String epost){
+        this.epost = epost;
+
+    }
+
+
+}
